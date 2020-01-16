9e75b2d872804f91930ccf57e88fe5c69fbea5a0
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-01-15 22:55:32 -0300

    Adição de todo o layout da aplicação + responsividade

--- src/App.js
+++ src/App.js
@@ -1,21 +1,92 @@
 import React from 'react';
+import './Global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input type="text" name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input type="text" name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="text" name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="text" name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Samuel Leonel</strong>
+                <span>Angular, Vue JS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Samuel Leonel</strong>
+                <span>Angular, Vue JS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Samuel Leonel</strong>
+                <span>Angular, Vue JS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Samuel Leonel</strong>
+                <span>Angular, Vue JS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
+            <a href="">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
