147942574b299dfa18a390458edb31013a996b13
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-01-16 22:25:34 -0300

    Separação da estrutura e adição de funcionalidade de adição de dev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 import './Global.css';
 import './App.css';
 import './Sidebar.css';
@@ -6,85 +11,35 @@ import './Main.css';
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data])
+  }
+
   return (
     <div id="App">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input type="text" name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input type="text" name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type="text" name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type="text" name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Samuel Leonel</strong>
-                <span>Angular, Vue JS</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
-            <a href="">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Samuel Leonel</strong>
-                <span>Angular, Vue JS</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
-            <a href="">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Samuel Leonel</strong>
-                <span>Angular, Vue JS</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
-            <a href="">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/36999289?s=460&v=4" alt=""/>
-              <div className="user-info">
-                <strong>Samuel Leonel</strong>
-                <span>Angular, Vue JS</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eum sapiente magnam mollitia, eveniet et! Repudiandae vero, corrupti odio beatae dignissimos omnis iste ut sequi adipisci incidunt, maiores tempore. Quos.</p>
-            <a href="">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
